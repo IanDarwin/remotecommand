1f75b2c3de77934039e44c1b9c561a7fb6446547
Ian Darwin <ian@darwinsys.com>
2018-02-27 09:30:11 -0500

    Prune dead method; extract remote object for dissection

--- client/src/main/java/remotecommand/client/Client.java
+++ client/src/main/java/remotecommand/client/Client.java
@@ -3,6 +3,7 @@ package remotecommand.client;
 import java.io.*;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.util.Arrays;
 
 import remotecommand.*;
 
@@ -25,20 +26,19 @@ public class Client {
 
 		// Find the server, and register with it
 		System.out.println("Finding server");
-		CommandProcessor server = 
-			(CommandProcessor)Naming.lookup("rmi://" + host + "/" +
-			Command.REGISTER_NAME);
+		Object remObj =
+			Naming.lookup("rmi://" + host + "/" + Command.REGISTER_NAME);
+		System.out.println("Remote object is a " + remObj.getClass().getName());
+		// System.out.println("It implements: ");
+		// Arrays.asList(remObj.getClass().getInterfaces()).forEach(System.out::println);
+		CommandProcessor server = (CommandProcessor)remObj;
 
 		// Create a Command and send it to the server
-		System.out.println("Connecting to server");
+		System.out.println("Sending a Command to the server");
 		server.submit(() -> System.out.println("Hello world"));
 
 		System.out.println("Command sent to server; look for output in server log.");
-	}
 
-	/** This is the client callback */
-	public void alert(String message) throws RemoteException {
-		System.out.println(message);
+		System.exit(0);		// BG threads may otherwise keep client running.
 	}
 }
-// END main
